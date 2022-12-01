b64f665c2b6ed1b5a75a2d98bd39a663cabfee26
suman kumar das <dsumankumar314@gmail.com>
2022-12-01 12:47:33 +0530

    add modifled file palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,13 +1,12 @@
 
 def isPalindrome(s):
     return s == s[::-1]
- 
- 
-#
+
 s = "TENET"
+print(s)
 ans = isPalindrome(s)
  
 if ans:
-    print(" yes string is palindrome")
+    print(" yes string is palindrome",ans)
 else:
-    print("No string is palindrome")
+    print("No string is palindrome",ans)
